a88df357205132addf58629b8df1e1a1d2512bd1
andy94077 <andy94077@gmail.com>
2018-07-01 22:13:53 +0800

    update bullet update method

--- Model/GameObject/bullet.py
+++ Model/GameObject/bullet.py
@@ -1,29 +1,27 @@
 import Model.const as modelConst
 import View.const as viewConst
-from white_ball import White_Ball
 class Bullet(object):
 	def __init__(self, pos,direction,index):
 		self.pos=pos
 		self.direction=direction
 		self.index = index
 		self.speed=modelConst.bullet_speed0
-	def update(self, wblist):
+	def update(self):
 		'''
 		return:
 			True: update success
 			False: update failed
 		'''
 		if (self.direction.x > 0 and self.pos.x >= viewConst.ScreenSize[0] - modelConst.eps) \
-			or (self.direction.x < 0 and self.pos.x <= 0 - modelConst.eps):
+			or (self.direction.x < 0 and self.pos.x <= -modelConst.eps):
 			self.direction.x *= -1
 		if (self.direction.y > 0 and self.pos.y >= viewConst.ScreenSize[1] - modelConst.eps) \
-			or (self.direction.y < 0 and self.pos.y <= 0 - modelConst.eps):
+			or (self.direction.y < 0 and self.pos.y <= -modelConst.eps):
 			self.direction.y *= -1
+		
 		self.pos += self.direction * self.speed
 		self.speed -= modelConst.bullet_a
-		if self.speed <= modelConst.eps:
-			wblist.append(White_Ball())
-			return False
-		return True
+		
+		return (self.speed > 0):
     
 		
