2cb7644617f66d3718fd1997a9585686823de692
BillKung <billkung.happy@gmail.com>
2018-07-01 15:40:45 +0800

    add init in Vec and add init_ball in white_ball_list

--- Model/GameObject/utils.py
+++ Model/GameObject/utils.py
@@ -2,6 +2,9 @@ class Vec():
 	def __init__(self,x,y):
 		self.x=x
 		self.y=y
+	def __init__(self,v):
+		self.x=v.x
+		self.y=v.y
 	def __str__(self):
 		return "("+str(self.x)+','+str(self.y)+")"
 	def __add__(self,that):
