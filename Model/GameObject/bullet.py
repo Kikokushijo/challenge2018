38155088cdd256d619ec863e03c62812ef3c759e
andy94077 <andy94077@gmail.com>
2018-07-01 21:58:35 +0800

    bullet init

---
+++ Model/GameObject/bullet.py
@@ -0,0 +1,10 @@
+import Model.const as modelConst
+class Bullet(object):
+	"""docstring for Bullet"""
+	def __init__(self, pos,dir,id):
+		self.pos=pos
+		self.dir=dir
+		self.id=id
+	def update():
+		self.pos+=
+		
