287881a198519a78236eef97d55cf43ceec98afc
andy94077 <andy94077@gmail.com>
2018-07-02 21:24:42 +0800

    revise class body to Body

--- Model/GameObject/body.py
+++ Model/GameObject/body.py
@@ -3,7 +3,7 @@ import View.const as viewconst
 import random
 from math import pi, sin, cos
 from pygame.math import Vector2 as Vec
-class body(object):
+class Body(object):
 	def __init__(self, pre):
 		self.pre = pre
 		self.index = pre.index
@@ -15,7 +15,7 @@ class body(object):
 		self.speed = pre.speed
 	def update(self):
 		self.pos += self.speed * self.direction
-		self.direction = pre.direction_list[0]
+		self.direction = self.pre.direction_list[0]
 		self.direction_list.append(self.direction)
 		if len(self.direction_list) > modelconst.max_delay_ticks:
 			self.direction_list.pop(0)
