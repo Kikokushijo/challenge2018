ff5a0b26cf9690793f040fc1e3c1bf9364df31d6
devin6011 <t45678h9@yahoo.com.tw>
2018-07-02 14:59:01 +0800

    Add some colors.

--- View/const.py
+++ View/const.py
@@ -5,11 +5,35 @@ ScreenSize = (800, 800)
 FramePerSec = 60
 
 # Color def
-Color_White = (255, 255, 255)
-Color_Black = (  0,   0,   0)
-Color_Blue  = (  0,   0, 255)
-Color_Green = (  0, 255,   0)
-Color_Red   = (255,   0,   0)
-Color_Gray  = (128, 128, 128)
 
-playerColor = [Color_Blue, Color_Green, Color_Red, Color_Black ]
+# Avoid using these boring colors
+Color_White       = (255, 255, 255)
+Color_Black       = (  0,   0,   0)
+Color_Blue        = (  0,   0, 255)
+Color_Green       = (  0, 255,   0)
+Color_Red         = (255,   0,   0)
+Color_Gray        = (128, 128, 128)
+
+Color_Turquoise   = ( 64, 224, 208)
+Color_Lightcoral  = (240, 128, 128)
+Color_Orangered   = (255,  69,   0)
+Color_Darkkhaki   = (189, 183, 107)
+Color_Gold        = (255, 215,   0)
+Color_Violet      = (238, 130, 238)
+Color_Darkviolet  = (148,   0, 211)
+Color_Limegreen   = ( 50, 205,  50)
+Color_Olive       = (128, 128,   0)
+Color_Royalblue   = ( 65, 105, 225)
+Color_Burlywood   = (222, 184, 135)
+Color_Silver      = (192, 192, 192)
+Color_Gainsboro   = (220, 220, 220)
+Color_Snow        = (255, 250, 250)
+
+# Object Color
+bgColor      =  Color_Gainsboro
+ballColor    =  Color_Snow
+playerColor  = [Color_Turquoise,  Color_Lightcoral, Color_Orangered, \
+                Color_Darkkhaki,  Color_Gold,       Color_Violet, \
+                Color_Darkviolet, Color_Limegreen,  Color_Olive, \
+                Color_Royalblue,  Color_Burlywood,  Color_Silver]
+
