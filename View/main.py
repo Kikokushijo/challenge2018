d4a8343118571c740780ef54ffc7ff70eed49aa3
devin6011 <t45678h9@yahoo.com.tw>
2018-07-04 13:03:18 +0800

    Polish the head

--- View/main.py
+++ View/main.py
@@ -1,5 +1,7 @@
 import pygame as pg
 import pygame.gfxdraw as draw
+from pygame.locals import *
+import math
 
 import Model.main as model
 from Events.Manager import *
@@ -26,6 +28,9 @@ class GraphicalView(object):
         self.screen = None
         self.clock = None
         self.smallfont = None
+
+        self.headIcon = None
+        self.headIcon2 = None
 
         self.last_update = 0
     
@@ -70,7 +75,19 @@ class GraphicalView(object):
         self.screen = pg.display.set_mode(viewConst.ScreenSize)
         self.clock = pg.time.Clock()
         self.smallfont = pg.font.Font(None, 40)
+
+        self.headIcon = pg.image.load('View/Image/headIcon.png').convert_alpha()
+        self.headIcon = pg.transform.scale(self.headIcon, (int(modelConst.head_radius * 1.3), \
+                         int(modelConst.head_radius * 1.3)))
+        self.headIcon2 = pg.image.load('View/Image/headIcon2.png').convert_alpha()
+        self.headIcon2 = pg.transform.scale(self.headIcon2, (int(modelConst.head_radius * 1.3), \
+                         int(modelConst.head_radius * 1.3)))
+
         self.is_initialized = True
+
+    def blit_at_center(self, surface, position):
+        (Xsize, Ysize) = surface.get_size()
+        self.screen.blit(surface, (int(position[0]-Xsize/2), int(position[1]-Ysize/2)))
 
     # to be modified
     def render_menu(self):
@@ -128,6 +145,7 @@ class GraphicalView(object):
         # draw scoreboard
         draw.vline(self.screen, viewConst.GameSize[0], 0, \
                    viewConst.GameSize[1], viewConst.sbColor)
+
         for i in range(1, 4):
             draw.hline(self.screen, viewConst.GameSize[0], \
                        viewConst.ScreenSize[0], \
@@ -135,22 +153,47 @@ class GraphicalView(object):
 
         for g in modelConst.grav:
             pos = (int(g[0][0]), int(g[0][1]))
-            r = int(g[1])
+            r = int(g[1] + modelConst.head_radius * 0.5)
             draw.filled_circle(self.screen, pos[0], pos[1], \
                                r, viewConst.gravColor)
             draw.filled_circle(self.screen, pos[0], pos[1], \
-                               4, viewConst.bgColor)
-
-        for player in self.model.player_list:
-            for body in player.body_list:
-                pos = (int(body.pos[0]), int(body.pos[1]))
-                draw.filled_circle(self.screen, pos[0], pos[1], \
-                                   int(body.radius), player.color)
+                               int(r * 0.07), viewConst.bgColor)
 
         for wb in self.model.wb_list:
             pos = (int(wb.pos[0]), int(wb.pos[1]))
             draw.filled_circle(self.screen, pos[0], pos[1], \
                                int(wb.radius), viewConst.wbColor)
+
+        for player in self.model.player_list:
+            for body in player.body_list[1:]:
+                pos = (int(body.pos[0]), int(body.pos[1]))
+                draw.filled_circle(self.screen, pos[0], pos[1], \
+                                   int(body.radius), player.color)
+
+        for player in self.model.player_list:
+            if player.is_alive:
+                pos = (int(player.pos[0]), int(player.pos[1]))
+                playerImage = pg.Surface((2 * int(player.radius), 2 * int(player.radius)), SRCALPHA)
+                draw.filled_circle(playerImage, int(player.radius), int(player.radius), \
+                                   int(player.radius), player.color)
+                if not player.is_dash:
+                    if player.is_circling:
+                        (Xsize, Ysize) = self.headIcon.get_size()
+                        playerImage.blit(self.headIcon, (int(player.radius - Xsize/2), int(player.radius - Ysize/2)))
+                    else:
+                        (Xsize, Ysize) = self.headIcon2.get_size()
+                        playerImage.blit(self.headIcon2, (int(player.radius - Xsize/2), int(player.radius - Ysize/2)))
+                    playerImage = pg.transform.rotate(playerImage, (player.theta - math.pi/2) * 180.0 / math.pi)
+                else:
+                    maxDashTime = modelConst.max_dash_time * modelConst.dash_speed_multiplier
+                    if not maxDashTime // 4 < player.dash_timer < maxDashTime // 4 * 3:
+                        (Xsize, Ysize) = self.headIcon2.get_size()
+                        playerImage.blit(self.headIcon2, (int(player.radius - Xsize/2), int(player.radius - Ysize/2)))
+                    scalingFactor = abs(math.cos(player.dash_timer / maxDashTime * 2 * math.pi))
+                    playerImage = pg.transform.scale(playerImage, (2 * int(player.radius * scalingFactor), 2 * int(player.radius)))
+                    if scalingFactor > 0.1:
+                        playerImage = pg.transform.rotate(playerImage, (player.theta - math.pi/2) * 180.0 / math.pi)
+                self.blit_at_center(playerImage, pos)
 
         for bullet in self.model.bullet_list:
             color = self.model.player_list[bullet.index].color
