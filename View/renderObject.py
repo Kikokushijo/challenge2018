feeb08f5509c704c18558c628bda966b028e45db
devin6011 <t45678h9@yahoo.com.tw>
2018-07-07 15:27:55 +0800

    Add bullet flash switch and end match scene

--- View/renderObject.py
+++ View/renderObject.py
@@ -47,6 +47,19 @@ class CountDown(RenderObject):
         super().__init__(pos, time, False)
 
 class MovingScore(RenderObject):
+    """
+    Class for showing scores in the end-game scenes.
+    """
     def __init__(self, index, pos, time):
         super().__init__(pos, time)
         self.index = index
+
+class Thermometer(RenderObject):
+    """
+    Class for showing scores in the end-match scene.
+    """
+    def __init__(self, index, pos, time, color, value):
+        super().__init__(pos, time)
+        self.index = index
+        self.color = color
+        self.value = value
