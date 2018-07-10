c53dd09502d9873e4033c60d021b577fa4e601ee
andy94077 <andy94077@gmail.com>
2018-07-10 11:33:30 +0800

    init is_going_collide in team_player1

--- AI/team_player1.py
+++ AI/team_player1.py
@@ -2,17 +2,45 @@ from AI.base import *
 from pygame.math import Vector2 as Vec
 
 import random
-
+eps=1e-5
 class TeamAI( BaseAI ):
     def __init__( self , helper ):
         self.helper = helper
         self.skill = []
-        wb_radius = helper.getWhiteballRadius()
-
-    def decide( self ):
-        helper = self.helper
-        hPos = helper.getMyHeadPos()
+        self.wb_radius = self.helper.getWhiteballRadius()
+        self.me = self.helper.model.player_list[self.helper.index]
+    
+    def is_going_collide(self, bullet):
+        x1, y1 = self.me.pos
+        v1x, v1y = self.me.direction * self.me.speed
+        s1=self.me.speed
+        x2, y2 = bullet.pos
+        v2x, v2y = bullet.direction * bullet.speed
+        s2=bullet.speed
         
+        delta = -v1x * v2y + v2x * v1y
+        if abs(delta) <= eps:
+            return False
+        collision_t1 = ((x2 - x1) * (-v2y) + v2x * (y2 - y1)) / delta
+        collision_t2 = (v1x * (y2 - y1) - (x2 - x1) * v2x) / delta
+        if collision_t1 <= -eps or collision_t2 <= -eps:
+            return False
+        
+        intersect_point = self.me.pos + self.me.direction * self.me.speed * collision_t1
+        a = self.me.pos.distance_to(Vec(intersect_point))#me_dist_to_point
+        b = bullet.pos.distance_to(Vec(intersect_point))  #bullet_dist_to_point
+        v1_dot_v2=self.me.direction.dot(bullet.direction)
+        D = 4 * ((a * s1 + b * s2)** 2 - (a ** 2 - 2 * a * b * v1_dot_v2 + b ** 2 - (self.me.radius + bullet.radius)** 2) * (s1 ** 2 - 2 * s1 * s2 * v1_dot_v2 + s2 ** 2))
+        if D < 0:
+            return False
+
+        
+    def decide( self ):
+        for bullet in self.helper.getAllPlayerBullet():
+            if self.helper.checkMeInGrav():
+                pass
+            else:
+                delta=
         if helper.getOtherBulletNumInRange(hPos, 10 * wb_radius) > 0:
             return AI_MoveWayChange
         if not helper.checkMeInGrav():
@@ -26,37 +54,3 @@ class TeamAI( BaseAI ):
             if not helper.checkMeCircling() and helper.canGetBySpin() > 0:
                 return AI_MoveWayChange
         return AI_NothingToDo
-        # hPos = helper.getMyHeadPos()
-        # hDir = helper.getMyDir()
-        # wb_radius = helper.getWhiteballRadius()
-        # if not helper.checkMeInGrav():
-        #     body_list = helper.bodyOnRoute()
-        #     if body_list:
-        #         dPos = helper.getDashPos()
-        #         if dPos is None:
-        #             return AI_NothingToDo
-        #         for bPos in body_list:
-        #             if (bPos - hPos).length() + 3 * wb_radius < (dPos - hPos).length():
-        #                 return AI_MoveWayChange
-
-        #     head_list = helper.bodyOnRoute()
-        #     if head_list:
-        #         dPos = helper.getDashPos()
-        #         if dPos is None:
-        #             return AI_NothingToDo
-        #         for Pos in head_list:
-        #             if (Pos - hPos).length() + 3 * wb_radius < (dPos - hPos).length():
-        #                 return AI_MoveWayChange
-        # else :
-        #     gPos, gRadius = helper.getMyGrav()
-        #     if not helper.checkMeCircling():
-        #         if hDir.dot(gPos - hPos) < 0:
-        #             ball = helper.canGetBySpin()
-        #             if ball > 0:
-        #                 return AI_MoveWayChange
-        #     else :
-        #         ball = helper.canGetBySpin()
-        #         if ball == 0:
-        #                 return AI_MoveWayChange
-
-        # return AI_NothingToDo
